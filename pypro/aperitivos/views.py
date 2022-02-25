658a59487a0d9c808a93308ff26a298f64801c4d
Aislanfagundes <64966358+Aislanfagundes@users.noreply.github.com>
2022-02-24 21:51:40 -0300

    subindo os contexto de template

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video={'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': "681076510?h=af518b33fe"}
+    return render(request, 'aperitivos/video.html', context={'video': video})
